e244ec21285ed9c1ceff92a0bafec4a52f43e31c
Bruno Felipy <brunofelipems@outlook.com>
2024-03-01 17:07:26 -0300

    feat: add list handler logic

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -4,10 +4,16 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/nobruf/gopportunities/schemas"
 )
 
 func ListOpeningsHandler(ctx *gin.Context){
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET Openings",
-	})
+	openings :=[]schemas.Opening{}
+
+	if err := db.Find(&openings).Error;err!=nil{
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+
+	sendSucess(ctx, "list-openings", openings)
 }
