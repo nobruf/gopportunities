f329f69c9181d86f3da24d31a82cc6455702db71
Bruno Felipy <brunofelipems@outlook.com>
2024-03-01 16:24:19 -0300

    feat: add createOpening handler logic

--- handler/handler.go
+++ handler/handler.go
@@ -1,8 +1,21 @@
 package handler
 
+import (
+	"github.com/nobruf/gopportunities/config"
+	"gorm.io/gorm"
+)
 
 
+var (
+	logger *config.Logger
+	db *gorm.DB
+)
 
+
+func InitializeHandler(){
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
 
 
 
