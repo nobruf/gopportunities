bc98bee2008c95ab6e49363c371110db12597e32
Bruno Felipy <brunofelipems@outlook.com>
2024-03-01 18:00:40 -0300

    docs: add anothers routes

--- handler/response.go
+++ handler/response.go
@@ -33,3 +33,19 @@ type CreateOpeningResponse struct {
 	Message string `json: "message"`
 	Data schemas.OpeningResponse `json: "data"`
 }
+type DeleteOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+type ShowOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+type ListOpeningsResponse struct {
+	Message string                    `json:"message"`
+	Data    []schemas.OpeningResponse `json:"data"`
+}
+type UpdateOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
